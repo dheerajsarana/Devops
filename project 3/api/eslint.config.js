aa070abe771a1ab20687eec6c84021eca9c26bcd
dheeraj <dheerajs@livegage.ai>
2026-09-18 16:28:32 +0530

    ci linting tests

---
+++ project 3/api/eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+    js.configs.recommended,
+];
